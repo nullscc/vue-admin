c41ff2debf884575180739d0d2f72c7e68b45076
hoo null <null@hoodeMacBook-Pro.local>
2018-04-29 14:42:45 +0800

    vue admin scaffold

---
+++ src/store/getters.js
@@ -0,0 +1,7 @@
+const getters = {
+  sidebar: state => state.app.sidebar,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name
+}
+export default getters
